fac854ba6e5d16a6a33f7ba19068312594c6ce82
Joocheol <joocheol@yonsei.ac.kr>
2020-05-08 15:36:38 +0900

    s

--- a20200507_10.py
+++ a20200507_10.py
@@ -3,35 +3,80 @@ from manimlib.imports import *
 class a20200507_10(GraphScene):
     
     CONFIG = {
-        "y_max" : 10,
-        "x_max" : 14,
-        "axes_color" : BLUE
+        "y_max" : 1,
+        "x_max" : 1,
+        "axes_color" : BLUE,
     }
 
     def construct(self):
 
         self.setup_axes(animate=True)
 
-        # graph = self.get_graph(
-        #     lambda x : np.sin(x), 
-        #     color = GREEN
-        # )
+        g = lambda x : 4 * x * (1-x)
 
-        # self.play(
-        #     ShowCreation(graph),
-        # )
-        # self.wait(1)
+        graph = self.get_graph(
+            g, 
+            color = GREEN
+        )
+
+        self.play(
+            ShowCreation(graph),
+        )
+        self.wait(1)
+
+        h = lambda x : x
+
+        graph = self.get_graph(
+            h, 
+            color = RED
+        )
+
+        self.play(
+            ShowCreation(graph),
+        )
+        self.wait(1)
 
 
         pt = Dot()
+        # pt.move_to(self.coords_to_point(0.5, 0.3))
+        # self.play(
+        #     ShowCreation(pt),
+        # )
+
+        # #pt.move_to(self.coords_to_point(0.5, 0.5))
+        # self.play(
+        #     ApplyMethod(pt.move_to, self.coords_to_point(0.5, 0.5) )
+        # )
+
+        
+        x = [0.1, 0.2, ]
+        x = 0.3
+        for i in range(30):
+            
+            self.play(ApplyMethod(pt.move_to, self.coords_to_point(x, g(x))))
+            self.play(ApplyMethod(pt.move_to, self.coords_to_point(g(x,), g(x))))
+            x = g(x)
+
+
+        self.graph_origin = RIGHT*2 + UP*2
+        self.setup_axes(animate=True)
 
         g = lambda x : 4 * x * (1-x)
-        #x = [0.1, 0.2, ]
-        #g_x = [g(x) for x in  ]
+
+        graph = self.get_graph(
+            g, 
+            color = GREEN
+        )
+
+        self.play(
+            ShowCreation(graph),
+        )
+        self.wait(1)
+
+        x = [0.1, 0.2, ]
         x = 0.3
-        for i in range(10):
+        for i in range(30):
             
-            self.play(ApplyMethod(pt.move_to, [x, g(x), 0]))
-            self.play(ApplyMethod(pt.move_to, [g(x), g(x), 0]))
+            self.play(ApplyMethod(pt.move_to, self.coords_to_point(x, g(x))))
+            self.play(ApplyMethod(pt.move_to, self.coords_to_point(g(x,), g(x))))
             x = g(x)
-                
--- test.py
+++ test.py
@@ -1,7 +1,52 @@
 from manimlib.imports import *
 
-class abc(Scene):
-	def construct(self):
-		a = TexMobject("abc", height=0.2, width=1.2)
+class aritmetik2(Scene):
+    def construct(self):
+        skouer_digomp = TexMobject(
+            "3960",
+            "=",
+            "2",
+            "\\times",
+            "2",
+            "\\times",
+            "2",
+            "\\times",
+            "3",
+            "\\times",
+            "3",
+            "\\times",
+            "5",
+            "\\times",
+            "11"
+        )
 
-		self.play(Write(a))
+        skouer_digomp_echu_simplaet = TexMobject("2^3", "\\times", "3^2", "\\times", "5", "\\times", "11")
+
+        etre_digomp = [
+            TexMobject("2", "\\times", "1980"),
+            TexMobject("2", "\\times", "990"),
+            TexMobject("2", "\\times", "495"),
+            TexMobject("3", "\\times", "165"),
+            TexMobject("3", "\\times", "55"),
+            TexMobject("5", "\\times", "11"),
+        ]
+
+        for i in range(5):
+            etre_digomp[i].set_color(RED)
+
+        self.play(Write(skouer_digomp[:2]))
+        self.wait()
+        self.play(Write(etre_digomp[0].next_to(skouer_digomp[:2])))
+        self.wait()
+
+
+        for i in range(5):
+            self.play(etre_digomp[i].set_color, WHITE, run_time=1)
+            self.wait()
+            self.play(etre_digomp[i][2].set_color, RED, run_time=1)
+            self.wait()
+            #self.play(ReplacementTransform(etre_digomp[i][2], etre_digomp[i+1].next_to(etre_digomp[i][2], RIGHT, buff=-0.5, aligned_edge=LEFT)))
+            self.play(ReplacementTransform(etre_digomp[i][2], etre_digomp[i+1].next_to(etre_digomp[i][2], RIGHT,buff=-0.3, aligned_edge=LEFT)))
+            self.wait()
+
+        self.wait(5)
