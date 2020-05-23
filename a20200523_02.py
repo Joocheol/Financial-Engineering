94449583d988c43d6d5c02bc4e7de20f10d8b229
Joocheol <joocheol@yonsei.ac.kr>
2020-05-23 13:50:18 +0900

    m

--- a20200523_02.py
+++ a20200523_02.py
@@ -3,17 +3,71 @@ from manimlib.imports import *
 class a20200523_02(Scene):
     def construct(self):
 
+        title = title_1_setup()
+        self.play(Write(title))
+        self.wait()
+        self.play(FadeOut(title))
+
         things = setup()
         things.scale(0.7)
-
         self.play(ShowCreation(things))
+        self.wait()
 
         things2 = setup()
         things2.scale(0.2).to_edge(UL,)
         self.play(Transform(things, things2))
 
-        self.play(Restore(things))
+        title = title_2_setup()
+        self.play(Write(title))
+        self.wait()
+        self.play(FadeOut(title))
+
+        title = title_3_setup()
+        self.play(Write(title))
+        self.wait()
+        self.play(FadeOut(title))
+
+        title = title_4_setup()
+        self.play(Write(title))
+        self.wait()
+        self.play(FadeOut(title))
+
+        title = title_5_setup()
+        self.play(Write(title))
+        self.wait()
+        self.play(FadeOut(title))
+
+        title = title_6_setup()
+        self.play(Write(title))
+        self.wait()
+        self.play(FadeOut(title))
+
+def title_6_setup():
+    text = TexMobject(r"""C - P &= {1 \over r} \Big[ p [ C_u - P_u ] + (1-p) [ C_d - P_d ] \Big] """)
+    return text
+
+def title_5_setup():
+    text = TexMobject(r"""C &= {1 \over r} \Big[ p C_u + (1-p) C_d \Big] \\ 
+        - P &= {1 \over r} \Big[ p P_u + (1-p) P_d \Big] """)
+    return text
+
+def title_4_setup():
+    text = TextMobject("Can you calculate $C - P$? ")
+    return text
+
+
+def title_3_setup():
+    text = TexMobject(r"P = {1 \over r} \Big[ p P_u + (1-p) P_d \Big] ")
+    return text
         
+def title_2_setup():
+    text = TextMobject("We know how to solve this problem")
+    return text
+    
+def title_1_setup():
+    text = TextMobject("Revisiting the games with the put option")
+    return text
+
 
 
 def setup():
@@ -33,8 +87,8 @@ def setup():
     r = VGroup(rr, tmp).arrange_submobjects(RIGHT, buff=1)
 
     CC = TexMobject("?")
-    CC_u = TexMobject("C_u")
-    CC_d = TexMobject("C_d")
+    CC_u = TexMobject("P_u")
+    CC_d = TexMobject("P_d")
 
     tmp = VGroup(CC_u, CC_d).arrange_submobjects(DOWN, buff=1)
     C = VGroup(CC, tmp).arrange_submobjects(RIGHT, buff=1)
