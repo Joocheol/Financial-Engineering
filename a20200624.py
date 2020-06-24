0b6daf01ce7544915e4973f98d22ab88af249a61
Joocheol <joocheol@yonsei.ac.kr>
2020-06-24 16:17:19 +0900

    m

--- a20200624.py
+++ a20200624.py
@@ -8,6 +8,14 @@ class a20200624(VectorScene):
 
     def construct(self):
 
-        grid = self.add_plane()
-        self.play(ShowCreation(grid))
-        self.wait()
+        #grid = self.add_plane()
+        #self.play(ShowCreation(grid))
+        #self.wait()
+
+        self.add_plane()
+        #self.add_axes()
+        #self.lock_in_faded_grid()
+        self.add_vector([1,2])
+        #v = Vector([0,1])
+        #self.write_vector_coordinates(v)
+        self.coords_to_vector([1,2])
