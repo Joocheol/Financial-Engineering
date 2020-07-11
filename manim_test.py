fb250dad062454bb3b1e1c6606ddac64223616e2
Joocheol <joocheol@yonsei.ac.kr>
2020-07-11 10:24:44 +0900

    w

--- manim_test.py
+++ manim_test.py
@@ -11,8 +11,16 @@ class manim_test(Scene):
 
         c = Circle()
         c.add_tip()
+        c.add(b)
         self.play(ShowCreation(c))
 
         print(a.points)
         print(b.points)
         print(c.points)
+
+        print(a.submobjects)
+        print(b.submobjects)
+        print(c.submobjects)
+
+        print(vars(Mobject))
+        print(dir(a))
