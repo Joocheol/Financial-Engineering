9985a7cd13eae33f0484e3ba18343c3e34b8a5ba
Joocheol <joocheol@yonsei.ac.kr>
2020-07-12 06:20:11 +0900

    s

--- manim_test.py
+++ manim_test.py
@@ -3,24 +3,12 @@ from manimlib.imports import *
 class manim_test(Scene):
     def construct(self):
 
-        a = Dot()
-        b = TextMobject("Hello")
-        a.surround(b)
+        # a = Circle(color=YELLOW)
+        # a.point_at_angle(0.3)
+        # self.play(ShowCreation(a))
+
+        b = Arc(num_components=4)
         self.play(ShowCreation(b))
-        self.play(ShowCreation(a))
-
-        c = Circle()
-        c.add_tip()
-        c.add(b)
-        self.play(ShowCreation(c))
-
-        print(a.points)
         print(b.points)
-        print(c.points)
 
-        print(a.submobjects)
-        print(b.submobjects)
-        print(c.submobjects)
-
-        print(vars(Mobject))
-        print(dir(a))
+        
