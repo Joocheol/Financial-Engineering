c4b75558e6ee40d7720bbf09f478ed3d9632ecd5
joocheol <joocheol@yonsei.ac.kr>
2021-04-17 15:10:20 +0900

    a

---
+++ .ipynb_checkpoints/Linear_Algebra_01-checkpoint.py
@@ -0,0 +1,189 @@
+from manimlib.imports import *
+
+class LA_01(Scene):
+    def construct(self):
+
+        text_010 = TextMobject("Can you solve the system of equations")
+
+        # self.play(GrowFromCenter(text_010))
+        # self.wait(1)
+
+        # self.play(FadeOut(text_010))
+        # self.wait(1)
+
+        formula_010 = TexMobject(r"""
+            x + y = 1  \\
+            x - y = 1 
+        """)
+
+        self.play(GrowFromCenter(formula_010))
+        self.wait(1)
+
+        text_030 = TextMobject("We know how to solve the system of equations.")
+        text_030.to_edge(DOWN)
+
+        self.play(Write(text_030))
+        self.wait()
+        self.play(FadeOut(text_030))
+
+        text_040 = TextMobject("One way is to use the graph.")
+        text_040.to_edge(DOWN)
+
+        self.play(Write(text_040))
+
+        formula_010.scale(0.7)
+        self.play(formula_010.to_edge, UL )
+
+        plane = NumberPlane()
+        self.add(plane)
+        self.wait()
+        self.play(FadeOut(text_040))
+
+        f1 = FunctionGraph(
+            lambda x: 1 - x,
+        )
+        self.play(ShowCreation(f1))
+
+        f2 = FunctionGraph(
+            lambda x: x - 1,
+        )
+        self.play(ShowCreation(f2))
+
+class LA_test(Scene):
+
+    print(color_to_rgba(Color('blue')))
+    print(rgb_to_color(np.array([0,1,0])))
+    print(random_bright_color())
+    
+
+class MyScene(Scene):
+
+    def add_vector(self, vector, color=YELLOW, animate=True, **kwargs):
+        if not isinstance(vector, Arrow):
+            vector = Vector(vector, color=color, **kwargs)
+        if animate:
+            self.play(GrowArrow(vector))
+        self.add(vector)
+        return vector
+
+    def write_vector_coordinates(self, vector, **kwargs):
+        coords = vector_coordinate_label(vector, **kwargs)
+        #self.play(Write(coords))
+        self.add(coords)
+        return coords
+
+class Linear_Algebra_01(Scene):
+    def construct(self):
+
+        self.intro()
+
+    def intro(self):
+        text_1 = TextMobject("``Cogito ergo sum.''")
+        #text_2 = TextMobject('English: "I think, therefore I am"')
+        text_3 = TextMobject("by Ren\\'{e} Descartes")
+        
+        self.play(Write(text_1))
+        #self.play(Write(text_2.next_to(text_1, DOWN)))
+        self.play(Write(text_3.next_to(text_1, DOWN)))
+        
+        self.wait()
+        return self
+
+class Linear_Algebra_02(VectorScene):
+    def construct(self):
+
+        v_1 = self.add_vector([-2,3])
+        self.write_vector_coordinates(v_1)
+        self.wait()
+        
+        self.vector_to_coords(v_1)
+
+        self.get_basis_vector_labels(at_tip=True)
+
+class Linear_Algebra_03(MyScene):
+    def construct(self):
+
+        plane = NumberPlane()
+
+        v_1 = self.add_vector([-2,-3])
+        self.write_vector_coordinates(v_1)
+        self.wait()
+        
+        #self.vector_to_coords(v_1)
+
+    #     v_2 = self.get_vector([1,2])
+
+    # def get_vector(self, numerical_vector, **kwargs):
+    #     return Arrow(
+    #         plane.coords_to_point(0, 0),
+    #         plane.coords_to_point(*numerical_vector[:2]),
+    #         buff=0,
+    #         **kwargs
+    #     )
+
+        self.tex("hello")
+
+class LA_04(Scene):
+    CONFIG = {
+        "quote": ["Cogito ergo sum."],
+        "quote_arg_separator": " ",
+        "highlighted_quote_terms": {},
+        "author": "Ren\\'{e} Descartes",
+        "fade_in_kwargs": {
+            "lag_ratio": 0.5,
+            "rate_func": linear,
+            "run_time": 5,
+        },
+        "text_size": "\\Large",
+        "use_quotation_marks": True,
+        "top_buff": 1.0,
+        "author_buff": 1.0,
+    }
+
+    def construct(self):
+        self.quote = self.get_quote()
+        self.author = self.get_author(self.quote)
+
+        self.play(FadeIn(self.quote, **self.fade_in_kwargs))
+        self.wait(2)
+        self.play(Write(self.author, run_time=3))
+        self.wait()
+
+    def get_quote(self, max_width=FRAME_WIDTH - 1):
+        text_mobject_kwargs = {
+            "alignment": "",
+            "arg_separator": self.quote_arg_separator,
+        }
+        if isinstance(self.quote, str):
+            if self.use_quotation_marks:
+                quote = TextMobject("``%s''" %
+                                    self.quote.strip(), **text_mobject_kwargs)
+            else:
+                quote = TextMobject("%s" %
+                                    self.quote.strip(), **text_mobject_kwargs)
+        else:
+            if self.use_quotation_marks:
+                words = [self.text_size + " ``"] + list(self.quote) + ["''"]
+            else:
+                words = [self.text_size] + list(self.quote)
+            quote = TextMobject(*words, **text_mobject_kwargs)
+            # TODO, make less hacky
+            if self.quote_arg_separator == " ":
+                quote[0].shift(0.2 * RIGHT)
+                quote[-1].shift(0.2 * LEFT)
+        for term, color in self.highlighted_quote_terms:
+            quote.set_color_by_tex(term, color)
+        quote.to_edge(UP, buff=self.top_buff)
+        if quote.get_width() > max_width:
+            quote.set_width(max_width)
+        return quote
+
+    def get_author(self, quote):
+        author = TextMobject(self.text_size + " --" + self.author)
+        author.next_to(quote, DOWN, buff=self.author_buff)
+        author.set_color(YELLOW)
+        return author
+
+
+    
+
---
+++ .ipynb_checkpoints/Risk_Neutral_Valuation-checkpoint.py
@@ -0,0 +1,137 @@
+from manimlib.imports import *
+
+class Risk_Neutral_Valuation(Scene):
+    def construct(self):
+
+        title = TextMobject("3. Risk Neutral Valuation")
+
+        self.play(GrowFromCenter(title))
+        self.wait(2)
+        self.play(FadeOut(title))
+
+        text = TextMobject("Let's start with numbers.")
+        text.to_edge(DOWN)
+
+        self.play(Write(text))
+        self.wait(1)
+
+        formula = TexMobject(
+            "22 \\times ", 
+            "\\frac{\\frac{9}{22}}{\\frac{9}{22}+\\frac{11}{22}} ",
+            "\\times ",
+            "(\\frac{_9}{^{22}}+\\frac{_{11}}{^{22}})",
+            " + 11 \\times ",
+            "\\frac{\\frac{11}{22}}{\\frac{9}{22}+\\frac{11}{22}} ",
+            "\\times ",
+            "(\\frac{_9}{^{22}}+\\frac{_{11}}{^{22}}) "
+        )
+
+        self.play(Write(formula))
+        self.wait(1)
+
+        self.play(formula.move_to, 2*UP)
+        self.wait(1)
+
+        self.play(FadeOut(text))
+
+        text = TextMobject("We have seen all these numbers, complicated though.")
+        text.to_edge(DOWN)
+
+        self.play(Write(text))
+        self.wait(1)
+
+        self.play(FadeOut(text))
+
+        text = TextMobject("Notice that these parts can be cancelled out.")
+        text.to_edge(DOWN)
+
+        brace1 = Brace(formula[1], DOWN, buff = SMALL_BUFF)
+        brace2 = Brace(formula[3], DOWN, buff = SMALL_BUFF)
+        brace3 = Brace(formula[5], DOWN, buff = SMALL_BUFF)
+        brace4 = Brace(formula[7], DOWN, buff = SMALL_BUFF)
+
+        self.play(
+            Write(text),
+            GrowFromCenter(brace1),
+            GrowFromCenter(brace2),
+            GrowFromCenter(brace3),
+            GrowFromCenter(brace4)
+        )
+        self.wait(2)
+
+        self.play(FadeOut(text))
+
+        text = TextMobject("There is a reason to keep them here.")
+        text.to_edge(DOWN)
+
+        self.play(
+            Write(text),
+            FadeOut(brace1),
+            FadeOut(brace2),
+            FadeOut(brace3),
+            FadeOut(brace4)
+        )
+        self.wait(1)
+
+        self.play(FadeOut(text))
+        text = TextMobject("The answer to this equation is 14.5 as usual.")
+        text.to_edge(DOWN)
+        self.play(Write(text))
+        self.wait(1)
+
+        self.play(FadeOut(text))
+        text = TextMobject("Each element in the equation has a special meaning.")
+        text.to_edge(DOWN)
+        self.play(Write(text))
+        self.wait(1)
+
+        self.play(FadeOut(text))
+        text = TextMobject("What are these?")
+        text.to_edge(DOWN)
+
+        framebox1 = SurroundingRectangle(formula[1], buff = .1)
+        framebox2 = SurroundingRectangle(formula[5], buff = .1)
+
+        self.play(
+            Write(text),
+            ShowCreation(framebox1),
+            ShowCreation(framebox2),
+        )
+        self.wait(2)
+
+        self.play(FadeOut(text), FadeOut(framebox1), FadeOut(framebox2),)
+        text = TextMobject("What are these?")
+        text.to_edge(DOWN)
+
+        framebox1 = SurroundingRectangle(formula[3], buff = .1)
+        framebox2 = SurroundingRectangle(formula[7], buff = .1)
+
+        self.play(Write(text), ShowCreation(framebox1), ShowCreation(framebox2))
+        self.wait(2)
+
+        self.play(FadeOut(text), FadeOut(framebox1), FadeOut(framebox2),)
+        text = TextMobject("What are these?")
+        text.to_edge(DOWN)
+
+        framebox1 = SurroundingRectangle(formula[0], buff = .1)
+        framebox2 = SurroundingRectangle(formula[4], buff = .1)
+
+        self.play(Write(text), ShowCreation(framebox1), ShowCreation(framebox2))
+        self.wait(2)
+
+        self.play(FadeOut(text), FadeOut(framebox1), FadeOut(framebox2),)
+        text = TextMobject("With all these, we have the risk neutral valuation formula.")
+        text.to_edge(DOWN)
+
+        formula = TextMobject(
+            "\\textrm{df} ", 
+            "E",
+            "(X)"
+        )
+
+        self.play(Write(formula))
+        self.wait(1)
+        self.play(Write(text))
+        self.wait(2)
+
+
